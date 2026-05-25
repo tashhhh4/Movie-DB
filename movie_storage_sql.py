5f32304e2c77dd2e8f54d940b5b6d3143715858b
Natasha Libera <tash@artoftash.com>
2026-05-25 17:46:13 +0200

    Added list and add functions

--- movie_storage_sql.py
+++ movie_storage_sql.py
@@ -18,3 +18,34 @@ def ensure_schema():
         
         """))
         connection.commit()
+
+
+def list_movies():
+    """ Retrieve all movies from the database. """
+    with engine.connect() as connection:
+        result = connection.execute(text("""
+        
+            SELECT title, year, rating FROM movies
+        
+        """))
+        movies = result.fetchall()
+
+    return {row[0]: {"year": row[1], "rating": row[2]} for row in movies}
+
+
+def add_movie(title, year, rating):
+    """ Add a new movie to the database. """
+    with engine.connect() as connection:
+        try:
+            connection.execute(text("""
+            
+                INSERT INTO movies (title, year, rating)
+                VALUES (:title, :year, :rating)
+            
+            """),
+            {"title": title, "year": year, "rating": rating})
+            connection.commit()
+            print(f"Movie '{title}' added successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+    
--- run_sql.py
+++ run_sql.py
@@ -1,5 +1,12 @@
 # Runs and tests sql queries
 
-from movie_storage_sql import ensure_schema
+from movie_storage_sql import ensure_schema, add_movie, list_movies
 
 ensure_schema()
+
+add_movie("The Devil Wears Prada", 2006, 6.5)
+add_movie("The Butterfly Effect", 2004, 8.4)
+
+movies = list_movies()
+for movie in movies:
+    print(movie)
