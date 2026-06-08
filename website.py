d943d88380d9bb8a1dba9ae871a4f5abcc822f6d
Natasha Libera <tash@artoftash.com>
2026-06-08 16:25:08 +0200

    Generated webpage works with or without a user now.

--- website.py
+++ website.py
@@ -40,14 +40,17 @@ def get_movies():
 
 # Website Generator
 def generate():
-    # movies = [
-    #     {"title": "Titanic", "poster_url": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", "rating": "8.0", "year": 1997},
-    #     {"title": "Titanic", "poster_url": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", "rating": "8.0", "year": 1997},
-    #     {"title": "Titanic", "poster_url": "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg", "rating": "8.0", "year": 1997},
-    # ]
     movies = get_movies()
 
     username = "Tasha"
+    # username = None
+
+    if username:
+        user_heading = f'{username}\'s Favorites'
+        filename = f"movies_{username}.html"
+    else:
+        user_heading = ''
+        filename = "movies.html"
     
     list_items = ''
     for movie in movies:
@@ -57,10 +60,10 @@ def generate():
     css = load_text(STYLE_FILE)
     html = html.replace("__TEMPLATE_TITLE__", APP_TITLE)
     html = html.replace("__CSS_STYLE__", css)
-    html = html.replace("__USERNAME__", username)
+    html = html.replace("__USER_HEADING__", user_heading)
     html = html.replace("__TEMPLATE_MOVIE_GRID__", list_items)
 
-    output_file = os.path.join(OUTPUT_DIR, f"{username}.html") 
+    output_file = os.path.join(OUTPUT_DIR, filename) 
     save_webpage(output_file, html)    
 
     print("Website was generated successfully.")
